3f0c4a29f4bee96c2fe536cc0b94c08c87e21e2e
jiaxin <1094630886@qq.com>
2021-09-13 17:41:00 +0800

    postgresql13使用rpm安装

---
+++ postgresql-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#安装依赖以及postgresql13.4
+rpm -uvh postgresql-rpm/* --nodeps --force
+#初始化数据库
+/usr/pgsql-13/bin/postgresql-13-setup initdb
+#设置开机自启
+systemctl enable postgresql-13
+#启动postgresql
+systemctl start postgresql-13 
