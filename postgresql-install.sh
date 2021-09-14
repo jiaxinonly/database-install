bd3ff428147615e58b2ca1567fc4e13e126eb6d9
jiaxinonly <1094630886@qq.com>
2021-09-14 11:14:49 +0800

    修复安装

--- postgresql-install.sh
+++ postgresql-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #安装依赖以及postgresql13.4
-rpm -uvh postgresql-rpm/* --nodeps --force
+rpm -Uvh postgresql-rpm/* --nodeps --force
 #初始化数据库
 /usr/pgsql-13/bin/postgresql-13-setup initdb
 #设置开机自启
